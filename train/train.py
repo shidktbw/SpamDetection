6122a2765191ce653097ff948ebb855f14c71eea
shidktbw <112849918+shidktbw@users.noreply.github.com>
2023-05-12 11:51:56 +0300

    Update train.py

--- train/train.py
+++ train/train.py
@@ -19,11 +19,10 @@ df = pd.read_csv('spam.csv', encoding=encoding)
 
 
 tokenizer = Tokenizer(num_words=10000, oov_token='<OOV>')
-tokenizer.fit_on_texts(df['v2']) # change 'message' to 'v2'
-sequences = tokenizer.texts_to_sequences(df['v2']) # change 'message' to 'v2'
+tokenizer.fit_on_texts(df['v2'])
+sequences = tokenizer.texts_to_sequences(df['v2']) 
 padded = pad_sequences(sequences, padding='post')
 
-# Change 'label' to 'v1' and make sure it is encoded properly (1 for spam and 0 for ham)
 df['v1'] = df['v1'].map({'ham': 0, 'spam': 1}) 
 
 X_train, X_test, y_train, y_test = train_test_split(padded, df['v1'], test_size=0.2, random_state=42) # change 'label' to 'v1'
@@ -37,5 +36,5 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 history = model.fit(X_train, y_train, epochs=1000, validation_data=(X_test, y_test), batch_size=32)
 
-# Сохранение модели
-model.save('new_model.h5')
+
+model.save('model.h5')
